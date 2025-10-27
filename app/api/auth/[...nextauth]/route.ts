e737edd9d88b82cdbee806949e4132aebe77b7a0
rbc33 <ricardobenthem@gmail.com>
2025-10-27 19:39:53 +0100

    add prisma adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,20 @@
 import NextAuth from 'next-auth/next'
 import { AuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from '@/prisma/client'
 
 export const authOptions: AuthOptions = {
+	adapter: PrismaAdapter(prisma),
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID!,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
 		}),
 	],
+	session: {
+		strategy: 'jwt',
+	},
 }
 
 const handler = NextAuth(authOptions)
